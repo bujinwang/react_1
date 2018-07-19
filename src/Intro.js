e49093cb3bab3e25544dfa19af64eb060bc5fa24
bujin <bujinwang@gmail.com>
2018-07-18 18:58:10 -0600

    seperate component to files

---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Intro extends Component {
+    render() {
+        return (
+            <p className="App-intro">
+                New Intro: To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        );
+    }
+}
+
+export default Intro;
